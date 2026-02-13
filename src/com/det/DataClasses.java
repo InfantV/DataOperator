cb8b154c7593534447653c2efe200c47e4d9ded5
InfantV <rajinfantgk@gmail.com>
2026-02-13 14:09:59 +0530

    updated the data operator

---
+++ src/com/det/DataClasses.java
@@ -0,0 +1,29 @@
+package com.det;
+
+public class DataClasses {
+	public static void main(String[] args) {
+		 
+		byte a = 120;
+		
+		short s= 2345;
+		
+		int i = 86874683;
+		
+		long l = 847588495845l;
+		
+		float f = 86487.3984783F;
+		
+		double d = 7468584758.67672;
+		
+		
+		char c ='g';
+		
+		String k= "ghhehjkrhg";
+		
+		boolean j=true;
+		
+		
+		
+	}
+
+}
---
+++ src/com/det/DatatypeScanner.java
@@ -0,0 +1,11 @@
+package com.det;
+
+import java.util.Scanner;
+
+public class DatatypeScanner {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
